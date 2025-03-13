080a1557e47ebe67eb6bf785500abcad25e67e91
Ewan Parker <34030942+ewpa@users.noreply.github.com>
2025-03-13 19:15:42 +0000

    Return library version number to sketch.  Bump version to 5.4.0.

--- src/libssh_esp32.h
+++ src/libssh_esp32.h
@@ -3,6 +3,10 @@
 // Ewan Parker, created 18th April 2020.
 // Arduino library header needed to compile and link libssh with ESP32.
 //
-// Copyright (C) 2020–2024 Ewan Parker.
+// Copyright (C) 2020–2025 Ewan Parker.
 
 void libssh_begin(void);
+
+#define LIBSSH_ESP32_VERSION_MAJOR 5
+#define LIBSSH_ESP32_VERSION_MINOR 4
+#define LIBSSH_ESP32_VERSION_PATCH 0
